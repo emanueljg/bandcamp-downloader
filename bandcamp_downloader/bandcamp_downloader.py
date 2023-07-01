983242be5d63808c2824b72b446ffe479f1592a2
emanueljg <emanueljohnsongodin@gmail.com>
2023-07-01 03:46:02 +0200

    fix typo

--- bandcamp_downloader/bandcamp_downloader.py
+++ bandcamp_downloader/bandcamp_downloader.py
@@ -323,7 +323,7 @@ def sanitize_path(_path : str) -> str:
 def get_cookies():
     if CONFIG['COOKIES']:
         cj = http.cookiejar.CookieJar()
-        with open(CONFIG['BROWSER']) as f:
+        with open(CONFIG['COOKIES']) as f:
             print(f.readlines())
             for line in f:
                 if line.startswith('.'):
